7ebf77d285011895659c00c1f2f64e44780b090d
Michael Milewski <saramic@gmail.com>
2018-04-09 22:20:31 +1000

    add md5 sign example
    based on PHP md5 sample from https://global.alipay.com/service/transaction_QR_Code/31?_rd=0.2586440247042141#DemoDownload

--- ali-pay/lib/ali_pay.rb
+++ ali-pay/lib/ali_pay.rb
@@ -1,4 +1,7 @@
 require 'json'
+require 'digest'
+require 'bigdecimal'
+require 'bigdecimal/util'
 
 class AliPay
   PARTNER = '2088021966388155'
@@ -17,6 +20,50 @@ class AliPay
     "total_fee"     => "100"
   }
 
+  def initialize(_input_charset: nil,
+                 extend_params: nil,
+                 notify_url: nil,
+                 price: nil,
+                 quantity: nil,
+                 show_url: nil,
+                 timestamp: nil,
+                 trans_currency: nil
+                )
+    @_input_charset = _input_charset
+    @extend_params = extend_params
+    @notify_url = notify_url
+    @price = price
+    @quantity = quantity
+    @show_url = show_url
+    @timestamp = timestamp
+    @trans_currency = trans_currency
+  end
+
+  def md5_sign
+    query_params = {
+      "_input_charset"      => @_input_charset,
+      "body"                => "iphone cellphone",
+      "currency"            => "USD",
+      "extend_params"       => @extend_params.to_json,
+      "notify_url"          => @notify_url,
+      "partner"             => PARTNER,
+      "passback_parameters" => "test",
+      "price"               => @price,
+      "product_code"        => "OVERSEAS_MBARCODE_PAY",
+      "quantity"            => @quantity,
+      "seller_email"        => "testoverseas1980@alipay.com",
+      "seller_id"           => PARTNER,
+      "service"             => "alipay.acquire.precreate",
+      "show_url"            => @show_url,
+      "subject"             => "Payment by QR-Code",
+      "timestamp"           => @timestamp,
+      "total_fee"           => @price, #(@price.to_d * @quantity).to_s,
+      "trans_currency"      => @trans_currency
+    }
+    query_params_string = hash_to_sorted_query_params(query_params)
+    Digest::MD5.hexdigest(query_params_string + PARTNER_KEY)
+  end
+
   def pay_url
     extended_parameters = {
       "secondary_merchant_name"     => "Lotte",
@@ -32,7 +79,7 @@ class AliPay
       "extend_params"       => extended_parameters.to_json,
       "notify_url"          => "http://api.test.alipay.net/atinterface/receive_notify.htm",
       "out_trade_no"        => "4363476566647440",
-      "partner"             => "2088021966388155",
+      "partner"             => PARTNER,
       "passback_parameters" => "test",
       "price"               => "0.01",
       "product_code"        => "OVERSEAS_MBARCODE_PAY",
@@ -51,6 +98,7 @@ class AliPay
     # TODO use proper URI or CGI to create query params
     [HTTPS_REQUEST_URL, query_params_string].join('?')
   end
+
 
   def pay
     hash_to_sorted_query_params(PARAMS)
--- ali-pay/spec/ali_pay_spec.rb
+++ ali-pay/spec/ali_pay_spec.rb
@@ -38,4 +38,24 @@ RSpec.describe AliPay do
 
     expect(AliPay.new.pay_url).to eq expected_api_url
   end
+
+  it 'sign a message' do
+    ali_pay = AliPay.new(
+      :_input_charset => "utf-8",
+      :extend_params  => {
+        "secondary_merchant_name"     => "Lotte",
+        "secondary_merchant_id"       => "123",
+        "secondary_merchant_industry" => "5812",
+        "store_id"                    => "A101",
+        "store_name"                  => "McDonald in 966 3rd Ave, New York"
+      },
+      :notify_url     => "http:\/\/\u5546\u6237\u7f51\u5173\u5730\u5740\/alipay.acquire.precreate-PHP-UTF-8-MD5\/notify_url.php",
+      :price          => "0.01",
+      :quantity       => "1",
+      :show_url       => "http:\/\/www.taobao.com\/product\/113714.html",
+      :timestamp      => "2018-04-09 10:43:38",
+      :trans_currency => "USD"
+    )
+    expect(ali_pay.md5_sign).to eq 'f1f75100c82dcfc40aa0c86477ba11a1'
+  end
 end
