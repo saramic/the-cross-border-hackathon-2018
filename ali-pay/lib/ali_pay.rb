837c66c96ee768da67aec8084fc29abbadc1beed
Michael Milewski <saramic@gmail.com>
2018-04-10 20:35:14 +1000

    added test RSA key and RSA sign_type but still ILLEGAL_PARTNER?

--- ali-pay/lib/ali_pay.rb
+++ ali-pay/lib/ali_pay.rb
@@ -2,12 +2,37 @@ require 'json'
 require 'digest'
 require 'bigdecimal'
 require 'bigdecimal/util'
+require 'alipay/sign/rsa'
 
 class AliPay
   PARTNER = '2088021966388155'
   PARTNER_KEY = 'w0nu2sn0o97s8ruzrpj64fgc8vj8wus6'
   HTTPS_REQUEST_URL = 'https://intlmapi.alipay.com/gateway.do' # production
   #HTTPS_REQUEST_URL = 'https://openapi.alipaydev.com/gateway.do' # test
+
+  APP_PRIVATE_KEY = """-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQD7FfmVdd570cFX3G76FatugmYlGNFtw9b2kFiBPvJDM0yeX52S
+WqAtjVBizaf6zNzib1a9xety7dnAxELidQJmUj3z4lwb3j5PRn+lx5FeAyAJWRBS
+m5+J3AzhOoHpStgATvEXT/AUndrgdn5Adfi45koUB8IzJzGZmk+5DabrIwIDAQAB
+AoGABd2WjgpJ30SgKqhIo2j8L0G2lylEQzcu/2WR27sXt8AsCwsayD97vHSvqw84
+3GAKzPNi6F4GSRILYzhyg43ON3/d3m/S1iTgSFXchTyAsAjy398Iq/ufSj9DOj+u
+I88XZ7JVt7jEyihPldSh7FkuMKaaFkJdsNz22LfD3NZIBVECQQD+I/zp+OuxmW5D
+GcNDEIXF0bl3mmCAv041ZBBO5JCYrgvpkofC6SRlOi+P3BPXYPHZjyU3Jp3XgaJD
+QvHCEkXbAkEA/OxD8FDM5i1JqcwIzha+NyJdm3nJ7Njlx4VIKD9Kx10oG49ge7gG
+8fDC7FwduGupfPj9ycKsHBFWC+AIUPSGWQJASqzWbwWWc+wKiyrxxi3/sL3MSomR
+G6Oa3nNVm2jr2AEkPPCydaI98ISlXxciuWGiDvRR0h/Ft4I+5JayJP1riwJAHe6o
+K6ykUylQ6nNnJ1yxrjy270t+hjU1HGO9omW7J9TyhjB1rq6cw81GhEpyifzT1iJx
+hosvazLyB/2/fjUyIQJBAJDzo5n2tVhsQEe5aNlF5ZHTtReyTMnPR4HgfBqGWydc
+DV/Wx7roFLX0dYT71CMD3vvHtda0qfNpQe8nM43lJfc=
+-----END RSA PRIVATE KEY-----
+"""
+
+  ALIPAY_PUBLIC_KEY = <<-EOF
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB
+
+-----END PUBLIC KEY-----
+EOF
 
   def initialize(_input_charset: nil,
                  extend_params: nil,
@@ -19,7 +44,8 @@ class AliPay
                  trans_currency: nil,
                  out_trade_no: nil,
                  seller_email: nil,
-                 sendFormat: nil
+                 sendFormat: nil,
+                 sign_type: nil
                 )
     @_input_charset = _input_charset
     @extend_params = extend_params
@@ -32,6 +58,7 @@ class AliPay
     @out_trade_no = out_trade_no
     @seller_email = seller_email
     @sendFormat = sendFormat
+    @sign_type = sign_type
   end
 
   def query_params
@@ -60,14 +87,18 @@ class AliPay
     params
   end
 
-  def md5_sign
+  def sign
     query_params_string = hash_to_sorted_query_params(query_params)
-    Digest::MD5.hexdigest(query_params_string + PARTNER_KEY)
+    if @sign_type == 'RSA'
+      Alipay::Sign::RSA.sign(APP_PRIVATE_KEY, query_params_string)
+    else
+      Digest::MD5.hexdigest(query_params_string + PARTNER_KEY)
+    end
   end
 
   def pay_url
     query_params_string = hash_to_sorted_query_params(query_params)
-    query_params_string = [query_params_string, "sign=#{md5_sign}"].join('&')
+    query_params_string = [query_params_string, "sign=#{sign}"].join('&')
     # TODO use proper URI or CGI to create query params
     [HTTPS_REQUEST_URL, query_params_string].join('?')
   end
--- ali-pay/spec/ali_pay_spec.rb
+++ ali-pay/spec/ali_pay_spec.rb
@@ -72,6 +72,36 @@ RSpec.describe AliPay do
       :trans_currency => "USD",
       :seller_email   => "testoverseas1980@alipay.com"
     )
-    expect(ali_pay.md5_sign).to eq 'f1f75100c82dcfc40aa0c86477ba11a1'
+    expect(ali_pay.sign).to eq 'f1f75100c82dcfc40aa0c86477ba11a1'
+  end
+
+  it 'RSA signs a message' do
+    ali_pay = AliPay.new(
+      :sign_type      => 'RSA',
+      :_input_charset => "utf-8",
+      :extend_params  => {
+        "secondary_merchant_name"     => "Lotte",
+        "secondary_merchant_id"       => "123",
+        "secondary_merchant_industry" => "5812",
+        "store_id"                    => "A101",
+        "store_name"                  => "McDonald in 966 3rd Ave, New York"
+      },
+      :notify_url     => "http:\/\/\u5546\u6237\u7f51\u5173\u5730\u5740\/alipay.acquire.precreate-PHP-UTF-8-MD5\/notify_url.php",
+      :price          => "0.01",
+      :quantity       => "1",
+      :show_url       => "http:\/\/www.taobao.com\/product\/113714.html",
+      :timestamp      => "2018-04-09 10:43:38",
+      :trans_currency => "USD",
+      :seller_email   => "testoverseas1980@alipay.com"
+    )
+    expect(ali_pay.sign).to eq 'bN1bZPiNBGzNjbwXa2mL2Zxf/p4lJnv6T9zi6To+lA6VqHld/o/oAhwT+fM4N5XYMwUgog0kqkuD9PZBVxqnzWztgUh0R5gaHQWoMK/F2doIq7APaQydLMpNcX7ETbexI48uWW64SR3aqiTA6nIhCmzyI7+5YDEokYuFRNYUggM='
+    # puts ali_pay.pay_url
+    #   https://openapi.alipaydev.com/gateway.do?_input_charset=utf-8&body=iphone%20cellphone&currency=USD&extend_params={%22secondary_merchant_name%22:%22Lotte%22,%22secondary_merchant_id%22:%22123%22,%22secondary_merchant_industry%22:%225812%22,%22store_id%22:%22A101%22,%22store_name%22:%22McDonald%20in%20966%203rd%20Ave,%20New%20York%22}&notify_url=http://%E5%95%86%E6%88%B7%E7%BD%91%E5%85%B3%E5%9C%B0%E5%9D%80/alipay.acquire.precreate-PHP-UTF-8-MD5/notify_url.php&partner=2088021966388155&passback_parameters=test&price=0.01&product_code=OVERSEAS_MBARCODE_PAY&quantity=1&seller_email=testoverseas1980@alipay.com&seller_id=2088021966388155&service=alipay.acquire.precreate&show_url=http://www.taobao.com/product/113714.html&subject=Payment%20by%20QR-Code&timestamp=2018-04-09%2010:43:38&total_fee=0.01&trans_currency=USD&sign=bN1bZPiNBGzNjbwXa2mL2Zxf/p4lJnv6T9zi6To+lA6VqHld/o/oAhwT+fM4N5XYMwUgog0kqkuD9PZBVxqnzWztgUh0R5gaHQWoMK/F2doIq7APaQydLMpNcX7ETbexI48uWW64SR3aqiTA6nIhCmzyI7+5YDEokYuFRNYUggM=&sign_type=RSA
+    #
+    # returns
+    # <alipay>
+    #   <is_success>F</is_success>
+    #   <error>ILLEGAL_PARTNER</error>
+    # </alipay
   end
 end
