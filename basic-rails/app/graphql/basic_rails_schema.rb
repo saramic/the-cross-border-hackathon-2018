3c7a49fa87f8c14f6f2b5b2b3e00601433f063de
Michael Milewski <saramic@gmail.com>
2018-04-15 23:03:45 +1000

    added graphql and generated basic schema
    rails generate graphql:install
    following along with https://blog.codeship.com/how-to-implement-a-graphql-api-in-rails/

---
+++ basic-rails/app/graphql/basic_rails_schema.rb
@@ -0,0 +1,4 @@
+BasicRailsSchema = GraphQL::Schema.define do
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
--- basic-rails/config/routes.rb
+++ basic-rails/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  post "/graphql", to: "graphql#execute"
   root 'high_voltage/pages#show', id: 'root'
 
   resources :fx_rates, only: %w(show index)
