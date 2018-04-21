03c4e853e759b110e8e109d7d1469f01a4878b2f
Michael Milewski <saramic@gmail.com>
2018-04-21 12:59:21 +1000

    ali-pay url generation now actually works woot!

--- documentation/demo/ali-pay/lib/ali_pay.rb
+++ documentation/demo/ali-pay/lib/ali_pay.rb
@@ -3,102 +3,63 @@ require 'digest'
 require 'bigdecimal'
 require 'bigdecimal/util'
 require 'alipay/sign/rsa'
+require 'uri'
+require 'CGI'
 
 class AliPay
-  PARTNER = '2088021966388155'
-  PARTNER_KEY = 'w0nu2sn0o97s8ruzrpj64fgc8vj8wus6'
-  HTTPS_REQUEST_URL = 'https://intlmapi.alipay.com/gateway.do' # production
-  #HTTPS_REQUEST_URL = 'https://openapi.alipaydev.com/gateway.do' # test
+  # HTTPS_REQUEST_URL = 'https://intlmapi.alipay.com/gateway.do' # production
+  # HTTPS_REQUEST_URL = 'https://openapi.alipaydev.com/gateway.do' # test
+  HTTPS_REQUEST_URL = 'https://mapi.alipay.com/gateway.do' # demo code
 
-  APP_PRIVATE_KEY = """-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQD7FfmVdd570cFX3G76FatugmYlGNFtw9b2kFiBPvJDM0yeX52S
-WqAtjVBizaf6zNzib1a9xety7dnAxELidQJmUj3z4lwb3j5PRn+lx5FeAyAJWRBS
-m5+J3AzhOoHpStgATvEXT/AUndrgdn5Adfi45koUB8IzJzGZmk+5DabrIwIDAQAB
-AoGABd2WjgpJ30SgKqhIo2j8L0G2lylEQzcu/2WR27sXt8AsCwsayD97vHSvqw84
-3GAKzPNi6F4GSRILYzhyg43ON3/d3m/S1iTgSFXchTyAsAjy398Iq/ufSj9DOj+u
-I88XZ7JVt7jEyihPldSh7FkuMKaaFkJdsNz22LfD3NZIBVECQQD+I/zp+OuxmW5D
-GcNDEIXF0bl3mmCAv041ZBBO5JCYrgvpkofC6SRlOi+P3BPXYPHZjyU3Jp3XgaJD
-QvHCEkXbAkEA/OxD8FDM5i1JqcwIzha+NyJdm3nJ7Njlx4VIKD9Kx10oG49ge7gG
-8fDC7FwduGupfPj9ycKsHBFWC+AIUPSGWQJASqzWbwWWc+wKiyrxxi3/sL3MSomR
-G6Oa3nNVm2jr2AEkPPCydaI98ISlXxciuWGiDvRR0h/Ft4I+5JayJP1riwJAHe6o
-K6ykUylQ6nNnJ1yxrjy270t+hjU1HGO9omW7J9TyhjB1rq6cw81GhEpyifzT1iJx
-hosvazLyB/2/fjUyIQJBAJDzo5n2tVhsQEe5aNlF5ZHTtReyTMnPR4HgfBqGWydc
-DV/Wx7roFLX0dYT71CMD3vvHtda0qfNpQe8nM43lJfc=
------END RSA PRIVATE KEY-----
-"""
-
-  ALIPAY_PUBLIC_KEY = <<-EOF
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB
-
------END PUBLIC KEY-----
-EOF
-
-  def initialize(_input_charset: nil,
-                 extend_params: nil,
-                 notify_url: nil,
-                 price: nil,
-                 quantity: nil,
-                 show_url: nil,
-                 timestamp: nil,
-                 trans_currency: nil,
-                 out_trade_no: nil,
-                 seller_email: nil,
-                 sendFormat: nil,
-                 sign_type: nil
-                )
-    @_input_charset = _input_charset
-    @extend_params = extend_params
-    @notify_url = notify_url
-    @price = price
-    @quantity = quantity
-    @show_url = show_url
-    @timestamp = timestamp
-    @trans_currency = trans_currency
-    @out_trade_no = out_trade_no
-    @seller_email = seller_email
-    @sendFormat = sendFormat
+  def initialize(
+      out_trade_no: nil,
+      sign_type:    'MD5'
+  )
+    default_params = {
+        _input_charset: 'utf-8',
+        # transport: 'http',           # as per PHP demo code
+        body: 'iphone cellphone',    # Specific description of the trade
+        currency: 'USD',             # Settlement currency
+        out_trade_no: nil,           # Unique order ID in Alipay’s merchant website
+        partner: 2088021966388155,
+        passback_parameters: 'test', # Alipay will feekback this parameter by the asynchronous notify
+        extend_params: {             # Be used for transmitting specific business information of the merchant
+                                     "secondary_merchant_name"     => "Lotte",
+                                     "secondary_merchant_id"       => "123",
+                                     "secondary_merchant_industry" => "5812",
+                                     "store_id"                    => "A101",
+                                     "store_name"                  => "McDonald in 966 3rd Ave, New York"
+        }.to_json,
+        # TODO change price to mandatory input
+        price: 0.01,                 # 0.01 # Unit price of the goods in the order
+        product_code:
+            'OVERSEAS_MBARCODE_PAY', # Order placement used to distinguish the business type
+        # TODO change quantity to mandatory input
+        quantity: 1,                 # Quantity of the goods in the order
+        seller_email:                # Seller's Alipay account
+                   'testoverseas1980@alipay.com',
+        seller_id: 2088021966388155, # Unique Alipay user ID corresponding to Seller’s Alipay account 16 numbers beginning with 2088
+        show_url:                    # Hyperlink for the show of goods on the webpage of checkout counter
+            'http://www.taobao.com/product/113714.html',
+        subject:
+            'Payment by QR-Code',    # Goods title/trade tile/order subject/order key word etc
+        # TODO change to be calculated
+        total_fee: 0.01,             # Total fee of this order
+        trans_currency: 'USD',       # Pricing currency for the transaction
+        notify_url:                  # Alipay server takes the initiative to notify merchant of webpage http path designated by website
+            'http://商户网关地址/alipay.acquire.precreate-PHP-UTF-8-MD5/notify_url.php',
+        service: "alipay.acquire.precreate"
+    }
+    @params = default_params.merge(
+        out_trade_no: out_trade_no,
+        timestamp: Time.now.getlocal('+08:00').strftime('%F %T') # TODO do this right on sign and send?
+    )
     @sign_type = sign_type
   end
 
-  def query_params
-    params = {
-      "_input_charset"      => @_input_charset,
-      "body"                => "iphone cellphone",
-      "currency"            => "USD",
-      "extend_params"       => @extend_params.to_json,
-      "notify_url"          => @notify_url,
-      "partner"             => PARTNER,
-      "passback_parameters" => "test",
-      "price"               => @price,
-      "product_code"        => "OVERSEAS_MBARCODE_PAY",
-      "quantity"            => @quantity,
-      "seller_email"        => @seller_email,
-      "seller_id"           => PARTNER,
-      "service"             => "alipay.acquire.precreate",
-      "show_url"            => @show_url,
-      "subject"             => "Payment by QR-Code",
-      "total_fee"           => @price, #(@price.to_d * @quantity).to_s,
-      "trans_currency"      => @trans_currency
-    }
-    params = params.merge("timestamp" => @timestamp) if @timestamp
-    params = params.merge("out_trade_no" => @out_trade_no) if @out_trade_no
-    params = params.merge("sendFormat" => @sendFormat) if @sendFormat
-    params
-  end
-
-  def sign
-    query_params_string = hash_to_sorted_query_params(query_params)
-    if @sign_type == 'RSA'
-      Alipay::Sign::RSA.sign(APP_PRIVATE_KEY, query_params_string)
-    else
-      Digest::MD5.hexdigest(query_params_string + PARTNER_KEY)
-    end
-  end
-
   def pay_url
-    query_params_string = hash_to_sorted_query_params(query_params)
-    query_params_string = [query_params_string, "sign=#{sign}"].join('&')
+    query_params_string = hash_to_sorted_query_params(@params)
+    query_params_string = [query_params_string, "sign=#{sign}", "sign_type=#{@sign_type}"].join('&')
     # TODO use proper URI or CGI to create query params
     [HTTPS_REQUEST_URL, query_params_string].join('?')
   end
@@ -111,6 +72,11 @@ EOF
       .to_h
       .inject([]) {|ary, (k, v)| ary << "#{k}=#{v}" }
       .join('&')
+  end
+
+  def sign
+    query_params_string = hash_to_sorted_query_params(@params)
+    Digest::MD5.hexdigest(query_params_string + ENV['ALIPAY_MD5'])
   end
 end
 
--- documentation/demo/ali-pay/spec/ali_pay_spec.rb
+++ documentation/demo/ali-pay/spec/ali_pay_spec.rb
@@ -1,107 +1,13 @@
 require 'spec_helper'
 require 'ali_pay'
+require 'timecop'
+
 RSpec.describe AliPay do
-  it 'creates the api url' do
-    expected_api_url = <<-EOF.gsub(/^\s+/, '').gsub(/\s+$/, '').gsub(/\R+/, '')
-      https://intlmapi.alipay.com/gateway.do?
-        _input_charset=UTF-8&
-        body=iphone cellphone&
-        currency=USD&
-        extend_params={
-          "secondary_merchant_name":"Lotte",
-          "secondary_merchant_id":"123",
-          "secondary_merchant_industry":"5812",
-          "store_id":"A101",
-          "store_name":"McDonald in 966 3rd Ave, New York"
-       }&
-       notify_url=http://api.test.alipay.net/atinterface/receive_notify.htm&
-       out_trade_no=4363476566647440&
-       partner=2088021966388155&
-       passback_parameters=test&
-       price=0.01&
-       product_code=OVERSEAS_MBARCODE_PAY&
-       quantity=1&
-       seller_email= testoverseas1980@alipay.com&
-       seller_id=2088021966388155&
-       sendFormat=normal&
-       service=alipay.acquire.precreate&
-       show_url=http://www.taobao.com/product/113714.html&
-       subject=Payment by QR-Code&
-       total_fee=0.01&
-       trans_currency=USD&
-       sign=18420f53772727a55457dd9e6f72cd00
-    EOF
-       # sign=2127020ad640f41eec725c639f1de294
-
-    ali_pay = AliPay.new(
-      :_input_charset => "UTF-8",
-      :extend_params  => {
-        "secondary_merchant_name"     => "Lotte",
-        "secondary_merchant_id"       => "123",
-        "secondary_merchant_industry" => "5812",
-        "store_id"                    => "A101",
-        "store_name"                  => "McDonald in 966 3rd Ave, New York"
-      },
-      :notify_url     => "http://api.test.alipay.net/atinterface/receive_notify.htm",
-      :price          => "0.01",
-      :quantity       => "1",
-      :show_url       => "http:\/\/www.taobao.com\/product\/113714.html",
-      :trans_currency => "USD",
-      :out_trade_no   => "4363476566647440",
-      :seller_email   => " testoverseas1980@alipay.com",
-      :sendFormat     => "normal"
-    )
-    expect(ali_pay.pay_url).to eq expected_api_url
-  end
-
-  it 'sign a message' do
-    ali_pay = AliPay.new(
-      :_input_charset => "utf-8",
-      :extend_params  => {
-        "secondary_merchant_name"     => "Lotte",
-        "secondary_merchant_id"       => "123",
-        "secondary_merchant_industry" => "5812",
-        "store_id"                    => "A101",
-        "store_name"                  => "McDonald in 966 3rd Ave, New York"
-      },
-      :notify_url     => "http:\/\/\u5546\u6237\u7f51\u5173\u5730\u5740\/alipay.acquire.precreate-PHP-UTF-8-MD5\/notify_url.php",
-      :price          => "0.01",
-      :quantity       => "1",
-      :show_url       => "http:\/\/www.taobao.com\/product\/113714.html",
-      :timestamp      => "2018-04-09 10:43:38",
-      :trans_currency => "USD",
-      :seller_email   => "testoverseas1980@alipay.com"
-    )
-    expect(ali_pay.sign).to eq 'f1f75100c82dcfc40aa0c86477ba11a1'
-  end
-
-  it 'RSA signs a message' do
-    ali_pay = AliPay.new(
-      :sign_type      => 'RSA',
-      :_input_charset => "utf-8",
-      :extend_params  => {
-        "secondary_merchant_name"     => "Lotte",
-        "secondary_merchant_id"       => "123",
-        "secondary_merchant_industry" => "5812",
-        "store_id"                    => "A101",
-        "store_name"                  => "McDonald in 966 3rd Ave, New York"
-      },
-      :notify_url     => "http:\/\/\u5546\u6237\u7f51\u5173\u5730\u5740\/alipay.acquire.precreate-PHP-UTF-8-MD5\/notify_url.php",
-      :price          => "0.01",
-      :quantity       => "1",
-      :show_url       => "http:\/\/www.taobao.com\/product\/113714.html",
-      :timestamp      => "2018-04-09 10:43:38",
-      :trans_currency => "USD",
-      :seller_email   => "testoverseas1980@alipay.com"
-    )
-    expect(ali_pay.sign).to eq 'bN1bZPiNBGzNjbwXa2mL2Zxf/p4lJnv6T9zi6To+lA6VqHld/o/oAhwT+fM4N5XYMwUgog0kqkuD9PZBVxqnzWztgUh0R5gaHQWoMK/F2doIq7APaQydLMpNcX7ETbexI48uWW64SR3aqiTA6nIhCmzyI7+5YDEokYuFRNYUggM='
-    # puts ali_pay.pay_url
-    #   https://openapi.alipaydev.com/gateway.do?_input_charset=utf-8&body=iphone%20cellphone&currency=USD&extend_params={%22secondary_merchant_name%22:%22Lotte%22,%22secondary_merchant_id%22:%22123%22,%22secondary_merchant_industry%22:%225812%22,%22store_id%22:%22A101%22,%22store_name%22:%22McDonald%20in%20966%203rd%20Ave,%20New%20York%22}&notify_url=http://%E5%95%86%E6%88%B7%E7%BD%91%E5%85%B3%E5%9C%B0%E5%9D%80/alipay.acquire.precreate-PHP-UTF-8-MD5/notify_url.php&partner=2088021966388155&passback_parameters=test&price=0.01&product_code=OVERSEAS_MBARCODE_PAY&quantity=1&seller_email=testoverseas1980@alipay.com&seller_id=2088021966388155&service=alipay.acquire.precreate&show_url=http://www.taobao.com/product/113714.html&subject=Payment%20by%20QR-Code&timestamp=2018-04-09%2010:43:38&total_fee=0.01&trans_currency=USD&sign=bN1bZPiNBGzNjbwXa2mL2Zxf/p4lJnv6T9zi6To+lA6VqHld/o/oAhwT+fM4N5XYMwUgog0kqkuD9PZBVxqnzWztgUh0R5gaHQWoMK/F2doIq7APaQydLMpNcX7ETbexI48uWW64SR3aqiTA6nIhCmzyI7+5YDEokYuFRNYUggM=&sign_type=RSA
-    #
-    # returns
-    # <alipay>
-    #   <is_success>F</is_success>
-    #   <error>ILLEGAL_PARTNER</error>
-    # </alipay
+  it 'just works' do
+    Timecop.freeze(DateTime.iso8601("2000-12-31T00:00:00+0000")) do
+      ENV['ALIPAY_MD5'] = 'XXXX'
+      ali_pay           = AliPay.new(out_trade_no: 1234, sign_type: 'MD5')
+      expect(ali_pay.pay_url).to eq('https://mapi.alipay.com/gateway.do?_input_charset=utf-8&body=iphone cellphone&currency=USD&extend_params={"secondary_merchant_name":"Lotte","secondary_merchant_id":"123","secondary_merchant_industry":"5812","store_id":"A101","store_name":"McDonald in 966 3rd Ave, New York"}&notify_url=http://商户网关地址/alipay.acquire.precreate-PHP-UTF-8-MD5/notify_url.php&out_trade_no=1234&partner=2088021966388155&passback_parameters=test&price=0.01&product_code=OVERSEAS_MBARCODE_PAY&quantity=1&seller_email=testoverseas1980@alipay.com&seller_id=2088021966388155&service=alipay.acquire.precreate&show_url=http://www.taobao.com/product/113714.html&subject=Payment by QR-Code&timestamp=2000-12-31 08:00:00&total_fee=0.01&trans_currency=USD&sign=6c478998267d747565aff1b8ab595380&sign_type=MD5')
+    end
   end
 end
