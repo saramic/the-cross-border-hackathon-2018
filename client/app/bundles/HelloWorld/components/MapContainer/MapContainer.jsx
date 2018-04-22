c3c8fb3c695fa1e573352ffc1c022430da4e599a
selenasmall <selenawiththetattoo@gmail.com>
2018-04-22 10:23:22 +1000

    spinner spinner, chicken dinner

--- client/app/bundles/HelloWorld/components/MapContainer/MapContainer.jsx
+++ client/app/bundles/HelloWorld/components/MapContainer/MapContainer.jsx
@@ -20,6 +20,10 @@ export class MapContainer extends React.Component {
       >
         <Marker onClick={this.onMarkerClick} name={'Current location'} />
         <Marker position={{lat: -16.8778, lng: 145.7499,}} name={'Cairns airport'} />
+        <Marker position={{lat: -16.944205, lng: 145.739841,}} name={'Cairns supermarket'} />
+        <Marker position={{lat: -16.938762, lng: 145.746183,}} name={'Cairns third address'} />
+        <Marker position={{lat: -16.933050, lng: 145.753282,}} name={'Cairns central ATM'} />
+        <Marker position={{lat: -16.926877, lng: 145.740750,}} name={'Cairns rando location'} />
 
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
